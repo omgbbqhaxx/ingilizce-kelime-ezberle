45c36cacc6be772d6688e5fdca7c97ec1bb8025d
Yasin Aktimur <chardone@windowslive.com>
2014-01-14 00:54:28 +0200

    regex tests

--- main.js
+++ main.js
@@ -1,45 +1,48 @@
+angular.module('App', []);
 
-var app = angular.module('MyTutorialApp',[]);
-app.controller("MainController", function($scope){
-	$scope.selectedPerson = 0;
-	$scope.selectedGenre = null;
+function Ctrl($scope) {
+  $scope.user = {name: 'guest', last: 'visitor'};
         $scope.Aralik = Math.floor(Math.random() * 4) + 0;
+	$scope.check = function() {
+		var re = new RegExp(this.peoples.code);
+		return re;
+		};
+		
         $scope.submit = function() {
             if (this.peoples.decode == this.text) {
             alert("Doğru");
-	    location.reload();
+         location.reload();
             }else{
-		alert("Hatalı");
-		location.reload();
-	    }
-	    
-	    };
+                alert("Hatalı");
+                location.reload();
+         }
         
-	$scope.people = [
-		{
-			id: 0,
-			code: 'merhaba',
+         };
+        
+        $scope.people = [
+                {
+                        id: 0,
+                        code: 'merhaba',
                         decode : 'hello'
-			
-		},
-		{
-			id: 1,
-			code: 'görmek',
-			decode : 'see'
-		},
+                        
+                },
                 {
-			id: 2,
-			code: 'asmak',
-			decode : 'hang'
-		},
+                        id: 1,
+                        code: 'görmek',
+                        decode : 'see'
+                },
                 {
-			id: 3,
-			code: 'detay',
-			decode : 'detail'
-		}
-		
-		
-	];
-
-    
-});
+                        id: 2,
+                        code: 'asmak',
+                        decode : 'hang'
+                },
+                {
+                        id: 3,
+                        code: 'detay',
+                        decode : 'detail'
+                }
+                
+                
+        ];
+  
+}
