482a193fb51ab542e9a9427402592b58116c7947
Yasin Aktimur <chardone@windowslive.com>
2014-01-13 17:37:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+
+var app = angular.module('MyTutorialApp',[]);
+app.controller("MainController", function($scope){
+	$scope.selectedPerson = 0;
+	$scope.selectedGenre = null;
+        $scope.Aralik = Math.floor(Math.random() * 4) + 0;
+        $scope.submit = function() {
+            if (this.text) {
+            alert(this.text);
+            this.text = '';
+            }};
+        
+	$scope.people = [
+		{
+			id: 0,
+			code: 'merhaba',
+                        decode : 'hello'
+			
+		},
+		{
+			id: 1,
+			code: 'görmek',
+			decode : 'see'
+		},
+                {
+			id: 2,
+			code: 'asmak',
+			decode : 'hang'
+		},
+                {
+			id: 3,
+			code: 'detay',
+			decode : 'detail'
+		}
+		
+		
+	];
+
+    
+});
