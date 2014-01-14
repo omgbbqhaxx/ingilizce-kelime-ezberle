a65cf54e581bfadafec6e4e194a910fa176c2d1a
Yasin Aktimur <chardone@windowslive.com>
2014-01-14 19:26:39 +0200

    added interval

--- main.js
+++ main.js
@@ -1,48 +1,43 @@
-angular.module('App', []);
-
-function Ctrl($scope) {
-  $scope.user = {name: 'guest', last: 'visitor'};
-        $scope.Aralik = Math.floor(Math.random() * 4) + 0;
-	$scope.check = function() {
-		var re = new RegExp(this.peoples.code);
-		return re;
-		};
-		
-        $scope.submit = function() {
-            if (this.peoples.decode == this.text) {
-            alert("Doğru");
-         location.reload();
-            }else{
-                alert("Hatalı");
-                location.reload();
-         }
+var app = angular.module('MyTutorialApp',[]);
+app.controller("MainController", function($scope,$interval){
+     $scope.Aralik = Math.floor(Math.random() * 4) + 0;
+    $scope.mol = "scope works!";
+    $interval(function() {
+        var x = $scope.people[$scope.Aralik]; 
+        if ($scope.google ==x.decode) {
+        location.reload();
         
-         };
-        
-        $scope.people = [
-                {
-                        id: 0,
-                        code: 'merhaba',
+        }
+     
+    }, 300);
+    
+    
+    
+    
+    $scope.people = [
+		{
+			id: 0,
+			code: 'merhaba',
                         decode : 'hello'
-                        
-                },
+			
+		},
+		{
+			id: 1,
+			code: 'görmek',
+			decode : 'see'
+		},
                 {
-                        id: 1,
-                        code: 'görmek',
-                        decode : 'see'
-                },
+			id: 2,
+			code: 'asmak',
+			decode : 'hang'
+		},
                 {
-                        id: 2,
-                        code: 'asmak',
-                        decode : 'hang'
-                },
-                {
-                        id: 3,
-                        code: 'detay',
-                        decode : 'detail'
-                }
-                
-                
-        ];
-  
-}
+			id: 3,
+			code: 'detay',
+			decode : 'detail'
+		}
+		
+		
+	];
+    
+});
