9f7206440392d5aeac7d80945e645648047c58a5
Yasin Aktimur <chardone@windowslive.com>
2014-01-15 00:16:08 +0200

    son

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var app = angular.module('MyTutorialApp',[]);
 app.controller("MainController", function($scope,$interval){
-	$scope.Aralik = Math.floor(Math.random() * 4) + 0;
-	$scope.mol = "Cevabı görmek için qqq yazın.";
+	$scope.Aralik = Math.floor(Math.random() * 9) + 0;
+	$scope.mol = "Cevabı görmek için qqq yazın. ip ucu içinse www";
 	$scope.people = [
 		  {
 			  id: 0,
@@ -23,6 +23,31 @@ app.controller("MainController", function($scope,$interval){
 			  id: 3,
 			  code: 'detay',
 			  decode : 'detail'
+		  },
+		  {
+			  id: 4,
+			  code: 'uyumak',
+			  decode : 'sleep'
+		  },
+		  {
+			  id: 5,
+			  code: 'seyehat',
+			  decode : 'travel'
+		  },
+		  {
+			  id: 6,
+			  code: 'bakmak',
+			  decode : 'look'
+		  },
+		  {
+			  id: 7,
+			  code: 'çağırmak',
+			  decode : 'call'
+		  },
+		  {
+			  id: 8,
+			  code: 'sevmek',
+			  decode : 'love'
 		  }
 		  
 		  
@@ -32,14 +57,19 @@ app.controller("MainController", function($scope,$interval){
         var x = $scope.people[$scope.Aralik];
 	switch ($scope.google) {
 		case x.decode:
-			$scope.Aralik = Math.floor(Math.random() * 4) + 0;
+			$scope.Aralik = Math.floor(Math.random() * 9) + 0;
 			$scope.google = "";
+			$scope.result = "";
 			break;
 		case "qqq":
 			$scope.result = x.code +"<=>"+ x.decode;
-			$scope.Aralik = Math.floor(Math.random() * 4) + 0;
+			$scope.Aralik = Math.floor(Math.random() * 9) + 0;
 			$scope.google = "";
 			break;
+		case "www":
+			var xnxxl = $scope.people[$scope.Aralik].decode.length;
+			$scope.result = $scope.people[$scope.Aralik].decode.charAt(0)+"***"+ $scope.people[$scope.Aralik].decode.charAt(xnxxl-1);
+			
 		default:
 			var GirilenHarfSayisi =$scope.google.length;
 			var KelimeninHarfSayisi = x.decode.length;
@@ -50,7 +80,7 @@ app.controller("MainController", function($scope,$interval){
 	}
         
      
-    }, 300);
+    }, 100);
     
     
     
